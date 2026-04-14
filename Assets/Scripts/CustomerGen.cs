ec6b89063457eb71ec6fc9ccf357e0330120419f
linnarelli <linnarelli@ucsd.edu>
2026-04-14 14:27:45 -0700

    Basic stuff
    Just added basic stuff so we can work on it once we figure out what we want to do.

---
+++ Assets/Scripts/CustomerGen.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CustomerGen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    void Scale(float scale)
+    {
+        
+    }
+
+    void Transform(Vector3 move)
+    {
+        
+    }
+}
