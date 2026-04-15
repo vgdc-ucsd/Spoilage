8a2dc7f841034a70e11b6d90a6847163b24227f4
Calvin Truong <cat020@ucsd.edu>
2026-04-14 18:58:03 -0700

    General Ingredient Class
    -Created general class with four different attributes based on what we can make ingredients.
    -Removed create asset menu
    -One subclass for dough so far

--- Assets/Scripts/Ingredients/Ingredients.cs
+++ Assets/Scripts/Ingredients/Ingredients.cs
@@ -1,37 +1,30 @@
 ﻿using UnityEngine;
 
-public abstract class IngredientData : MonoBehaviour
+public abstract class Ingredient
 {
-    [SerializeField]
-    private string _ingredientName;
-
-    [SerializeField]
+    private string _name;
+    private bool _needsCooking;
+    private int _cookTime;
     private bool _isSpoiled;
 
-    [SerializeField]
-    private bool _isGrillable;
+    public Ingredient(string name, bool needsCooking, int cookTime, bool isSpoiled)
+    {
+        _name = name;
+        _needsCooking = needsCooking;
+        _cookTime = cookTime;
+        _isSpoiled = isSpoiled;
+    }
 
-    public string IngredientName => _ingredientName;
-    public bool IsSpoiled => _isSpoiled;
-    public bool IsGrillable => _isGrillable;
+    public string Name => _name;
+    public bool NeedsCooking => _needsCooking;
+    public int CookTime => _cookTime;
+    public bool isSpoiled => _isSpoiled;
 }
 
-[CreateAssetMenu(fileName = "NewDough", menuName = "Ingredients/Dough")]
-public class Dough : IngredientData
+public class Dough : Ingredient
 {
-}
-
-[CreateAssetMenu(fileName = "NewMeat", menuName = "Ingredients/Meat")]
-public class Meat : IngredientData
-{
-}
-
-[CreateAssetMenu(fileName = "NewRoots", menuName = "Ingredients/Roots")]
-public class Roots : IngredientData
-{
-}
-
-[CreateAssetMenu(fileName = "NewCheese", menuName = "Ingredients/Cheese")]
-public class Cheese : IngredientData
-{
+    public Dough(string name, bool needsCooking, int cookTime, bool isSpoiled)
+        : base(name, needsCooking, cookTime, isSpoiled)
+    {
+    }
 }
