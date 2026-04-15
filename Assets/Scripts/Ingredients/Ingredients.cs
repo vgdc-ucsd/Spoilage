d09adecefa5d607efcf990149c2faf3a91d48688
Calvin Truong <cat020@ucsd.edu>
2026-04-14 18:35:12 -0700

    General Ingredient Class
    - Created an ingredient class with three general attributes
    - Created subclasses that uses the "create asset menu" feature to add ingredients based on the description.

--- Assets/Scripts/Ingredients/Ingredients.cs
+++ Assets/Scripts/Ingredients/Ingredients.cs
@@ -1,6 +1,37 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Ingredients
+public abstract class IngredientData : MonoBehaviour
 {
-    
+    [SerializeField]
+    private string _ingredientName;
+
+    [SerializeField]
+    private bool _isSpoiled;
+
+    [SerializeField]
+    private bool _isGrillable;
+
+    public string IngredientName => _ingredientName;
+    public bool IsSpoiled => _isSpoiled;
+    public bool IsGrillable => _isGrillable;
+}
+
+[CreateAssetMenu(fileName = "NewDough", menuName = "Ingredients/Dough")]
+public class Dough : IngredientData
+{
+}
+
+[CreateAssetMenu(fileName = "NewMeat", menuName = "Ingredients/Meat")]
+public class Meat : IngredientData
+{
+}
+
+[CreateAssetMenu(fileName = "NewRoots", menuName = "Ingredients/Roots")]
+public class Roots : IngredientData
+{
+}
+
+[CreateAssetMenu(fileName = "NewCheese", menuName = "Ingredients/Cheese")]
+public class Cheese : IngredientData
+{
 }
