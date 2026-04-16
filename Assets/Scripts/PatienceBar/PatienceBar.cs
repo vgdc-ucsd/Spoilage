3f4a549f9db398d7450553d43a600a996ae827b4
Samantha Machleit <machleit0225@gmail.com>
2026-04-15 18:31:12 -0700

    Added decay functionality + Events for PatienceExpired & PatienceIsUrgent
    Customizable decay speed and rate
    Events for any future parent objects to know when patience is depleted

--- Assets/Scripts/PatienceBar/PatienceBar.cs
+++ Assets/Scripts/PatienceBar/PatienceBar.cs
@@ -1,30 +1,70 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Rendering.Universal.Internal;
 using UnityEngine.UI;
 
 public class PatienceBar : MonoBehaviour
 {
     // wowow
+    // Threshold for warning, currently arbitrary
     private const float Urgent = 0.2f;
-
     [SerializeField] private Slider _slider;
     private bool _isUrgent;
+    private bool _isDecaying;
     private float _patience;
+    private float _decayRate;
+    private float _decayInterval;
+    private float _timer;
+
+    public UnityEvent PatienceExpired;
+    public UnityEvent PatienceIsUrgent;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _isUrgent = false;
+        PatienceExpired ??= new UnityEvent();
+        PatienceIsUrgent ??= new UnityEvent();
+        _patience = _slider.maxValue;
+        beginDecay(5, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if decay is active, lower patience
+        if (_isDecaying)
+        {
+            _timer = Time.deltaTime;
+            _patience -= (_timer / _decayInterval) * _decayRate;
+        }
+
+        // if patience is at or below 0, end decay and invoke event
+        if (_patience <= 0)
+        {
+            _patience = 0;
+            _isDecaying = false;
+            PatienceExpired.Invoke();
+        }
+
+        // updates slider value with current patience
         _slider.value = _patience;
-        if (_slider.normalizedValue < Urgent || !_isUrgent)
+
+        // if bar is below threshold make urgent
+        if (_slider.normalizedValue <= Urgent && !_isUrgent)
         {
             _isUrgent = true;
+            // this line will probably change once they decide what its gonna be
             _slider.fillRect.GetComponent<Image>().color = Color.red;
+            PatienceIsUrgent.Invoke();
+        }
+
+        // removes urgent status if above threshold
+        if (_slider.normalizedValue > Urgent && _isUrgent)
+        {
+            _isUrgent = false;
+            // this line will probably change once they decide what its gonna be
+            _slider.fillRect.GetComponent<Image>().color = Color.white;
         }
     }
 
@@ -48,10 +88,20 @@ public class PatienceBar : MonoBehaviour
     /// Number of patience points to reduce by each interval
     /// </param>
     /// <param name="interval">
-    /// Number of milliseconds between each interval
+    /// Number of seconds between each interval
     /// </param>
-    public void beginDecay(int rate, int interval) {
-        
+    public void beginDecay(float rate, float interval) {
+        _isDecaying = true;
+        _decayInterval = interval;
+        _decayRate = rate;
+    }
+
+    /// <summary>
+    /// Stops the decay of patience
+    /// </summary>
+    public void stopDecay()
+    {
+        _isDecaying = false;
     }
 
     /// <summary>
