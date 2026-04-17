6af57a6970d263afef87edfc91883e9e96dfe17e
OP-idk <ojparsley@gmail.com>
2026-04-16 22:02:52 -0700

    Sprite Selection is Working!
    I left in the debug stuff for now.
    Still missing logic for determining spoilage sprites based on the
    spoilage level since it's still uncertain if there will be mouth/eyes
    spoilage sprites and how we should handle that.

--- Assets/Scripts/CustomerManager.cs
+++ Assets/Scripts/CustomerManager.cs
@@ -1,79 +1,105 @@
 using System;
 using System.IO;
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class CustomerManager : MonoBehaviour
 {
-    private const string EYES_PATH = "Customers/Eyes/";
-    private const string NOSE_MOUTH_PATH = "Customers/NoseMouths/";
-    private const string SPOILAGE_PATH = "Customers/Spoilage/";
+    private const string EYES_PATH = "Assets/Resources/Customers/Eyes/";
+    private const string NOSE_MOUTH_PATH = "Assets/Resources/Customers/NoseMouths/";
+    private const string SPOILAGE_PATH = "Assets/Resources/Customers/Spoilage/";
     private const string FRONT_FOLDER = "Front/";
     private const string BACK_FOLDER = "Back/";
-    private const string BASES_PATH = "Customers/Bases/";
-    private const string CLOTHES_FOLDER = "Clothes/";
-    private const string HAIR_FOLDER = "Hair/";
+    private const string BASES_PATH = "Assets/Resources/Customers/Bases/";
+    private const string CLOTHES_FOLDER = "/Clothes/";
+    private const string HAIR_FOLDER = "/Hair/";
+
+    private const string PNG = "*.png";
 
     private const int HAIR_FRONT = 0;
     private const int HAIR_BACK = 1;
     private const int HAIR_SHADOW = 2;
 
-    
+
+    [SerializeField]
+    public CustomerData debug;
+
+    void Start()
+    {
+        debug = GenerateCustomerData();
+    }
+
+
     public static CustomerData GenerateCustomerData()
     {
-        CustomerData newData = new CustomerData
-        {
-            spoilage = (CustomerData.Spoilage) UnityEngine.Random.Range(0, 2),
-            sprites = new Sprite[CustomerData.NUM_SPRITES],
-            patience = UnityEngine.Random.Range(0f, 1f) // TODO, talk to design
-        };
+        CustomerData newData = ScriptableObject.CreateInstance<CustomerData>();
 
+        newData.spoilage = (CustomerData.Spoilage) UnityEngine.Random.Range(0, 2);
+        newData.sprites = new Sprite[CustomerData.NUM_SPRITES];
+        newData.patience = UnityEngine.Random.Range(0f, 1f); // TODO, talk to design
+
+        Debug.Log("Choosing eye directory");
         string eyesDir = getRandomElement(Directory.GetDirectories(EYES_PATH));
-        string[] eyesPaths = Directory.GetFiles(eyesDir);
+        Debug.Log(eyesDir);
+        string[] eyesPaths = Directory.GetFiles(eyesDir, PNG);
         Array.Sort(eyesPaths);
 
+        
         string eyesOpenPath = eyesPaths[0];
         string eyesClosedPath = eyesPaths[1];
 
-
-
-
+        Debug.Log("Choosing NoseMouth directory");
         string noseMouthDir = getRandomElement(Directory.GetDirectories(NOSE_MOUTH_PATH));
-        string[] noseMouthPaths = Directory.GetFiles(noseMouthDir);
+        string[] noseMouthPaths = Directory.GetFiles(noseMouthDir, PNG);
         Array.Sort(noseMouthPaths);
 
         string noseMouthOpenPath = noseMouthPaths[0];
         string noseMouthClosedPath = noseMouthPaths[1];
 
-        string spoilageFrontPath = getRandomElement(Directory.GetFiles(SPOILAGE_PATH + FRONT_FOLDER));
-        string spoilageBackPath = getRandomElement(Directory.GetFiles(SPOILAGE_PATH + BACK_FOLDER));
+        Debug.Log("Choosing spoilage front");        
+        string spoilageFrontPath = getRandomElement(Directory.GetFiles(SPOILAGE_PATH + FRONT_FOLDER, PNG));
+        Debug.Log("Choosing spoilage back");        
+        string spoilageBackPath = getRandomElement(Directory.GetFiles(SPOILAGE_PATH + BACK_FOLDER, PNG));
 
+        Debug.Log("Choosing body dir");
         string bodyDir = getRandomElement(Directory.GetDirectories(BASES_PATH));
-        string bodyPath = getRandomElement(Directory.GetFiles(bodyDir));
+        Debug.Log("Choosing body sprite");
+        string bodyPath = getRandomElement(Directory.GetFiles(bodyDir, PNG));
 
-        string clothesPath = getRandomElement(Directory.GetFiles(bodyDir + CLOTHES_FOLDER));
+        Debug.Log("Choosing clothes sprite");
+        string clothesPath = getRandomElement(Directory.GetFiles(bodyDir + CLOTHES_FOLDER, PNG));
 
+        Debug.Log("Choosing hair dir");
         string hairDir = getRandomElement(Directory.GetDirectories(bodyDir + HAIR_FOLDER));        
-        string[] hairPaths = Directory.GetFiles(hairDir);
+        string[] hairPaths = Directory.GetFiles(hairDir, PNG);
         Array.Sort(hairPaths);
+        Debug.Log(trimPath(bodyPath));
 
-        newData.sprites[(int)CustomerData.Indexes.BODY] = Resources.Load<Sprite>(bodyPath);
-        newData.sprites[(int)CustomerData.Indexes.HAIR_FRONT] = Resources.Load<Sprite>(hairPaths[HAIR_FRONT]);
-        newData.sprites[(int)CustomerData.Indexes.HAIR_BACK] = Resources.Load<Sprite>(hairPaths[HAIR_BACK]);
-        newData.sprites[(int)CustomerData.Indexes.HAIR_SHADOW] = Resources.Load<Sprite>(hairPaths[HAIR_SHADOW]);
-        newData.sprites[(int)CustomerData.Indexes.CLOTHES] = Resources.Load<Sprite>(clothesPath);
-        newData.sprites[(int)CustomerData.Indexes.NOSE_MOUTH_OPEN] = Resources.Load<Sprite>(noseMouthOpenPath);
-        newData.sprites[(int)CustomerData.Indexes.NOSE_MOUTH_CLOSED] = Resources.Load<Sprite>(noseMouthClosedPath);
-        newData.sprites[(int)CustomerData.Indexes.EYES_OPEN] = Resources.Load<Sprite>(eyesOpenPath);
-        newData.sprites[(int)CustomerData.Indexes.EYES_CLOSED] = Resources.Load<Sprite>(eyesClosedPath);
-        newData.sprites[(int)CustomerData.Indexes.SPOILAGE_FRONT] = Resources.Load<Sprite>(spoilageFrontPath);
-        newData.sprites[(int)CustomerData.Indexes.SPOILAGE_BACK] = Resources.Load<Sprite>(spoilageBackPath);
+        newData.sprites[(int)CustomerData.Indexes.BODY] = Resources.Load<Sprite>(trimPath(bodyPath));
+        //newData.sprites[(int)CustomerData.Indexes.BODY] = Resources.Load<Sprite>("Customers/Bases/BodyA/base287_body_C");
+        newData.sprites[(int)CustomerData.Indexes.HAIR_FRONT] = Resources.Load<Sprite>(trimPath(hairPaths[HAIR_FRONT]));
+        newData.sprites[(int)CustomerData.Indexes.HAIR_BACK] = Resources.Load<Sprite>(trimPath(hairPaths[HAIR_BACK]));
+        newData.sprites[(int)CustomerData.Indexes.HAIR_SHADOW] = Resources.Load<Sprite>(trimPath(hairPaths[HAIR_SHADOW]));
+        newData.sprites[(int)CustomerData.Indexes.CLOTHES] = Resources.Load<Sprite>(trimPath(clothesPath));
+        newData.sprites[(int)CustomerData.Indexes.NOSE_MOUTH_OPEN] = Resources.Load<Sprite>(trimPath(noseMouthOpenPath));
+        newData.sprites[(int)CustomerData.Indexes.NOSE_MOUTH_CLOSED] = Resources.Load<Sprite>(trimPath(noseMouthClosedPath));
+        newData.sprites[(int)CustomerData.Indexes.EYES_OPEN] = Resources.Load<Sprite>(trimPath(eyesOpenPath));
+        newData.sprites[(int)CustomerData.Indexes.EYES_CLOSED] = Resources.Load<Sprite>(trimPath(eyesClosedPath));
+        newData.sprites[(int)CustomerData.Indexes.SPOILAGE_FRONT] = Resources.Load<Sprite>(trimPath(spoilageFrontPath));
+        newData.sprites[(int)CustomerData.Indexes.SPOILAGE_BACK] = Resources.Load<Sprite>(trimPath(spoilageBackPath));
         
         return newData;
     }
 
     private static string getRandomElement(string[] arr)
     {
-        return arr[UnityEngine.Random.Range(0, arr.Length - 1)];
+        return arr[UnityEngine.Random.Range(0, arr.Length)];
+    }
+
+    private static string trimPath(string str)
+    {
+        int startIndex = "Assets/Resources/".Length;
+        int endOffset = ".png".Length;
+
+        return str.Substring(startIndex, str.Length - endOffset - startIndex);
     }
 }
