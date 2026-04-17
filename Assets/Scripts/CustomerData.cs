47f29dd8236733c7743e86466a260be5711dbc80
OP-idk <ojparsley@gmail.com>
2026-04-16 19:22:33 -0700

    Added logic for constructing sprites array
    Defined file system structure required for generator in
    Assets/Resources/Customers/README.md
    Has not been tested.

--- Assets/Scripts/CustomerData.cs
+++ Assets/Scripts/CustomerData.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class CustomerData : ScriptableObject
 {
-    public const int NUM_SPRITES = 8;
+    public const int NUM_SPRITES = 9;
+
+    /* Enum is actually kind of hard to work with since it can't be implicitly cast to int to actually use as indexes
+    public const int BODY_IDX = 0;
+    public const int HAIR_FRONT_IDX = 1;
+    public const int HAIR_BACK_IDX = 2;
+    public const int HAIR_SHADOW_IDX = 3;
+    public const int CLOTHES_IDX = 4;
+    public const int NOSE_MOUTH_IDX = 5;
+    public const int EYES_IDX = 6;
+    public const int SPOILAGE_FRONT_IDX = 7;
+    public const int SPOILAGE_BACK_IDX = 8;
+    */
     public enum Indexes
     {
-        BODY,
+        BODY = 0,
         HAIR_FRONT,
         HAIR_BACK,
+        HAIR_SHADOW,
+        CLOTHES,
+        NOSE_MOUTH,
+        EYES,
+        SPOILAGE_FRONT,
+        SPOILAGE_BACK
         // etc.
     }
+    
     public Sprite[] sprites;
     public Vector3[] spriteOffsets;
-    public float spoilage; // What range of values do we want for this?
+    public int spoilage; 
 }
--- Assets/Scripts/CustomerManager.cs
+++ Assets/Scripts/CustomerManager.cs
@@ -1,17 +1,68 @@
+using System;
+using System.IO;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class CustomerManager : MonoBehaviour
 {
-    public CustomerData generateCustomerData()
+    private const string EYES_PATH = "Customers/Eyes/";
+    private const string NOSE_MOUTH_PATH = "Customers/NoseMouths/";
+    private const string SPOILAGE_PATH = "Customers/Spoilage/";
+    private const string FRONT_FOLDER = "Front/";
+    private const string BACK_FOLDER = "Back/";
+    private const string BASES_PATH = "Customers/Bases/";
+    private const string CLOTHES_FOLDER = "Clothes/";
+    private const string HAIR_FOLDER = "Hair/";
+
+    private const int HAIR_FRONT = 0;
+    private const int HAIR_BACK = 1;
+    private const int HAIR_SHADOW = 2;
+
+    
+    public static CustomerData generateCustomerData()
     {
         CustomerData newData = new CustomerData
         {
-            spoilage = Random.Range(0, 1.0f),
+            spoilage = UnityEngine.Random.Range(0, 2),
             sprites = new Sprite[CustomerData.NUM_SPRITES],
             spriteOffsets = new Vector3[CustomerData.NUM_SPRITES]
         };
 
+        string eyesPath = getRandomElement(Directory.GetFiles(EYES_PATH));
+        string noseMouthPath = getRandomElement(Directory.GetFiles(NOSE_MOUTH_PATH));
+        string spoilageFrontPath = getRandomElement(Directory.GetFiles(SPOILAGE_PATH + FRONT_FOLDER));
+        string spoilageBackPath = getRandomElement(Directory.GetFiles(SPOILAGE_PATH + BACK_FOLDER));
+
+        string bodyDir = getRandomElement(Directory.GetDirectories(BASES_PATH));
+        string bodyPath = getRandomElement(Directory.GetFiles(bodyDir));
+
+        string clothesPath = getRandomElement(Directory.GetFiles(bodyDir + CLOTHES_FOLDER));
+
+        string hairDir = getRandomElement(Directory.GetDirectories(bodyDir + HAIR_FOLDER));        
+        string[] hairPaths = Directory.GetFiles(hairDir);
+        Array.Sort(hairPaths);
+
+
+        newData.sprites[(int)CustomerData.Indexes.BODY] = Resources.Load<Sprite>(bodyPath);
+        newData.sprites[(int)CustomerData.Indexes.HAIR_FRONT] = Resources.Load<Sprite>(hairPaths[HAIR_FRONT]);
+
+        newData.sprites[(int)CustomerData.Indexes.HAIR_BACK] = Resources.Load<Sprite>(hairPaths[HAIR_BACK]);
+        newData.sprites[(int)CustomerData.Indexes.HAIR_SHADOW] = Resources.Load<Sprite>(hairPaths[HAIR_SHADOW]);
+        newData.sprites[(int)CustomerData.Indexes.CLOTHES] = Resources.Load<Sprite>(clothesPath);
+        newData.sprites[(int)CustomerData.Indexes.NOSE_MOUTH] = Resources.Load<Sprite>(noseMouthPath);
+        newData.sprites[(int)CustomerData.Indexes.EYES] = Resources.Load<Sprite>(eyesPath);
+        newData.sprites[(int)CustomerData.Indexes.SPOILAGE_FRONT] = Resources.Load<Sprite>(spoilageFrontPath);
+        newData.sprites[(int)CustomerData.Indexes.SPOILAGE_BACK] = Resources.Load<Sprite>(spoilageBackPath);
+        
+
+
+
+
         return newData;
     }
 
+    private static string getRandomElement(string[] arr)
+    {
+        return arr[UnityEngine.Random.Range(0, arr.Length - 1)];
+    }
 }
