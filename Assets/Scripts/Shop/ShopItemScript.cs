d77b421c544b701f70bf3f95aeb51b784d8fa7e7
maxf6-283 <maxf6.283@gmail.com>
2026-04-16 21:09:10 -0700

    Add cursor change on hover

--- Assets/Scripts/Shop/ShopItemScript.cs
+++ Assets/Scripts/Shop/ShopItemScript.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class ShopItemScript : MonoBehaviour
 {
+    public Texture2D hoverCursor;
+
     public GameState gameState;
 
     public int price = 10;
@@ -21,13 +23,30 @@ public class ShopItemScript : MonoBehaviour
 
     }
 
+    bool canBuy()
+    {
+        return !_bought && gameState.Money >= price;
+    }
+
     void OnMouseDown()
     {
-        if (!_bought && gameState.Money >= price)
+        if (canBuy())
         {
             gameState.Money -= price;
             _bought = true;
             GetComponent<SpriteRenderer>().color = Color.gray;
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
         }
+    }
+
+    void OnMouseEnter()
+    {
+        if (canBuy())
+            Cursor.SetCursor(hoverCursor, new Vector2(hoverCursor.width, hoverCursor.height) / 2, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 }
