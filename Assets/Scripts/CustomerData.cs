e0950f30cab8aa6034ea1a625bd8995cc15887db
OP-idk <ojparsley@gmail.com>
2026-04-16 20:27:41 -0700

    Include mouth/nose variants

--- Assets/Scripts/CustomerData.cs
+++ Assets/Scripts/CustomerData.cs
@@ -2,19 +2,7 @@ using UnityEngine;
 
 public class CustomerData : ScriptableObject
 {
-    public const int NUM_SPRITES = 9;
-
-    /* Enum is actually kind of hard to work with since it can't be implicitly cast to int to actually use as indexes
-    public const int BODY_IDX = 0;
-    public const int HAIR_FRONT_IDX = 1;
-    public const int HAIR_BACK_IDX = 2;
-    public const int HAIR_SHADOW_IDX = 3;
-    public const int CLOTHES_IDX = 4;
-    public const int NOSE_MOUTH_IDX = 5;
-    public const int EYES_IDX = 6;
-    public const int SPOILAGE_FRONT_IDX = 7;
-    public const int SPOILAGE_BACK_IDX = 8;
-    */
+    public const int NUM_SPRITES = 11;
     public enum Spoilage
     {
         NOT,
@@ -28,8 +16,10 @@ public class CustomerData : ScriptableObject
         HAIR_BACK,
         HAIR_SHADOW,
         CLOTHES,
-        NOSE_MOUTH,
-        EYES,
+        NOSE_MOUTH_OPEN,
+        NOSE_MOUTH_CLOSED,
+        EYES_OPEN,
+        EYES_CLOSED,
         SPOILAGE_FRONT,
         SPOILAGE_BACK
         // etc.
