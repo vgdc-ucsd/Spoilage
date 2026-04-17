e31dfe2cbb35f90db356144d4d63e0e8700d30f2
sjiao729 <sjiao@ucsd.edu>
2026-04-16 20:44:06 -0700

    Copied over cooking script to Pot

---
+++ Assets/Scripts/Cooking Scripts/Pot.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    [SerializeField] private Timer _timer;
+
+    private IngredientObject _currentFood;
+    private bool _isCooking = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OnPlaceFood(FoodGrab food)
+    {
+        _currentFood = food.GetComponent<IngredientObject>();
+
+        if (_currentFood == null)
+        {
+            Debug.LogWarning("No IngredientObject found!");
+            return;
+        }
+
+        Debug.Log("Food on Grill");
+
+        if (_currentFood.IngredientInstance.CurrentState == IngredientState.Cooked)
+        {
+            Debug.Log("Food is already cooked");
+            return; 
+        }
+
+        // If the timer has time left and it's less than the total cook time, resume it.
+        if (_timer.TimeRemaining > 0 && _timer.TimeRemaining < _currentFood.IngredientInstance.Data.CookTime)
+        {
+            _isCooking = true;
+            _timer.ResumeTimer();
+            Debug.Log("Resuming timer at: " + _timer.TimeRemaining);
+        }
+        else
+        {
+            CookFood();
+        }
+
+    }
+
+    public void CookFood()
+    {
+
+        _isCooking = true;
+        _currentFood.IngredientInstance.CurrentState = IngredientState.Raw;
+        _timer.StartTimer(_currentFood.IngredientInstance.Data.CookTime);
+        Debug.Log("Started cooking");
+        
+    }
+
+    public void OnRemoveFood()
+    {
+        if (_isCooking)
+        {
+            _isCooking = false;
+            _timer.PauseTimer();
+            Debug.Log("Timer paused.");
+        }
+        _currentFood = null;
+    }
+
+    private void Update()
+    {
+        if (!_isCooking || _currentFood == null) return;
+
+        if (_timer.IsFinished())
+        {
+            FinishCooking();
+            //UpdateCookedFoodSprite();
+        }
+
+    }
+
+    private void FinishCooking()
+    {
+        _isCooking = false;
+        _currentFood.IngredientInstance.CurrentState = IngredientState.Cooked;
+        Debug.Log(_currentFood.IngredientInstance.Data.Name + " is now Cooked!");
+    }
+}
