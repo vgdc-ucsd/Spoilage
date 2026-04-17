78037baad822f7b7db4043ab1ee369a780bebd97
Abdulaziz Khader <130017155+aokhader@users.noreply.github.com>
2026-04-16 21:39:46 -0700

    Added saved data that persists across game sessions

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,34 +1,36 @@
-using System;
+﻿using System;
 using TMPro;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "GameState", menuName = "Scriptable Objects/GameState")]
 public class GameState : ScriptableObject
 {
-  [NonSerialized]
-  public TextMeshProUGUI moneyText = null;
+    [NonSerialized]
+    public TextMeshProUGUI moneyText = null;
 
-  [SerializeField]
-  private int _money;
+    [SerializeField]
+    private int _money;
 
-  public int Money
-  {
-    get { return _money; }
-    set
+    public int Money
     {
-      _money = value;
-      UpdateUI();
+        get { return _money; }
+        set
+        {
+            _money = value;
+            UpdateUI();
+            // If we have a master save manager, use instance.currentData.player.moneyGained
+            SaveManager.Instance.currentData.moneyGained = _money;
+        }
     }
-  }
 
   private void UpdateUI()
   {
-    if (moneyText)
-      moneyText.text = "$" + Money.ToString();
+      if (moneyText)
+          moneyText.text = "$" + Money.ToString();
   }
 
   private void OnValidate()
   {
-    UpdateUI();
+      UpdateUI();
   }
 }
