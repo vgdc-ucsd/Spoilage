c13a4c52a871ea67b43d2a8e7252f6dba11f8da9
williamchou500 <williamchou500@gmail.com>
2026-04-22 23:00:30 -0700

    created prefab for order display object
    still need to make it so that it can be modified depending on what item and ingredients are being requested

---
+++ Assets/Scenes/Testing/Order Display Object/DisplayObject.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class DisplayObject : MonoBehaviour
+{
+    public Transform canvas;
+    public GameObject displayObjectPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateReceipt(Item item)
+    {
+        
+    }
+}
