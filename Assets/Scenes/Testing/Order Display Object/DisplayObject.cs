54fdba20879d6649d184bcab90c86086adb4a58b
williamchou500 <williamchou500@gmail.com>
2026-04-28 19:37:51 -0700

    updated receipt design and script

--- Assets/Scenes/Testing/Order Display Object/DisplayObject.cs
+++ Assets/Scenes/Testing/Order Display Object/DisplayObject.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,18 +8,19 @@ public class DisplayObject : MonoBehaviour
     public Transform canvas;
     public GameObject ingredientImagePrefab;
     public int lineLength;
-    private int _numAsterisks;
+    private int _numDots;
     private int _numIngredients;
     public GameObject orderDisplayPrefab;
-    public GameObject orderObject;
-    public Item order;
+    private GameObject _orderObject;
+    private Item _order;
     private int _orderCostLength;
     private int _orderNameLength;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+    _orderObject = GameObject.FindGameObjectWithTag("Order");
+    _order = _orderObject.GetComponent<Item>();
     }
 
     // Update is called once per frame
@@ -27,59 +29,54 @@ public class DisplayObject : MonoBehaviour
         
     }
 
-    public void GenerateReceipt(Item item)
+    public void GenerateReceipt()
     {
+        Debug.Log("HI");
         if (GameObject.FindGameObjectWithTag("Order Display Object") != null)
         {
             Debug.Log("Order Display Object already exists");
             return;
         }
 
-        orderObject = GameObject.FindGameObjectWithTag("Order");
-        order = orderObject.GetComponent<Item>();
-
-        _numIngredients = order.ingredients.Count;
-        _orderCostLength = order.itemCost.ToString().Length;
-        _orderNameLength = order.itemName.Length;
-        _numAsterisks = lineLength - _orderCostLength - _orderNameLength;
+        _numIngredients = _order.ingredients.Count;
+        _orderCostLength = _order.itemCost.ToString().Length;
+        _orderNameLength = _order.itemName.Length;
+        _numDots = lineLength - _orderCostLength - _orderNameLength;
+        List<Texture2D> orderIngredients = _order.ingredients;
+        List<Texture2D> orderPreparations = _order.preparations;
 
         GameObject obj = Instantiate(orderDisplayPrefab, canvas);
         
         UnityEngine.UI.Image foodPicture = obj.transform.Find("Food Picture").GetComponent<UnityEngine.UI.Image>();
         TextMeshProUGUI itemName = obj.transform.Find("Item Name").GetComponent<TextMeshProUGUI>();
         Transform ingredients = obj.transform.Find("Ingredients");
-        UnityEngine.UI.Image barCode = obj.transform.Find("Barcode").GetComponent<UnityEngine.UI.Image>();
-        TextMeshProUGUI barCodeNumber = obj.transform.Find("Barcode Number").GetComponent<TextMeshProUGUI>();
-        UnityEngine.UI.Image signature = obj.transform.Find("Signature").GetComponent<UnityEngine.UI.Image>();
 
-        Sprite foodPictureSprite = Sprite.Create(order.foodPicture, new Rect(0, 0, 100, 100), new Vector2(0.5f, 0.5f), 100.0f);
+        Sprite foodPictureSprite = Sprite.Create(_order.foodPicture, new Rect(0, 0, 100, 100), new Vector2(0.5f, 0.5f), 100.0f);
         foodPicture.sprite = foodPictureSprite;
         foodPicture.color = Color.white;
         
-        itemName.text = order.itemName + new string('*', _numAsterisks) + order.itemCost.ToString();
+        itemName.text = _order.itemName + new string('.', _numDots) + _order.itemCost.ToString();
         
-        RectTransform rt = ingredients.GetComponent<RectTransform>();
-        rt.sizeDelta = new Vector2(rt.sizeDelta.x, _numIngredients * 100 + (_numIngredients - 1) * 5);
+        int overallHeight = 0;
 
-        foreach (Texture2D ingredient in order.ingredients)
+        for (int i = 0; i < _numIngredients; i++)
         {
-            GameObject ingredientImageObject = Instantiate(ingredientImagePrefab, ingredients);
+            GameObject ingredientContainer = Instantiate(ingredientImagePrefab, ingredients);
 
-            UnityEngine.UI.Image ingredientImage = ingredientImageObject.GetComponent<UnityEngine.UI.Image>();
+            UnityEngine.UI.Image ingredientImage = ingredientContainer.transform.Find("Ingredient").GetComponent<UnityEngine.UI.Image>();
+            UnityEngine.UI.Image preparationImage = ingredientContainer.transform.Find("Preparation").GetComponent<UnityEngine.UI.Image>();
 
-            Sprite ingredientSprite = Sprite.Create(ingredient, new Rect(0, 0, 400, 100), new Vector2(0.5f, 0.5f), 100.0f);
+            Sprite ingredientSprite = Sprite.Create(_order.ingredients[i], new Rect(0, 0, _order.ingredients[i].width, _order.ingredients[i].height), new Vector2(0.5f, 0.5f), 100.0f);
+            Sprite preparationSprite = Sprite.Create(_order.preparations[i], new Rect(0, 0, _order.preparations[i].width, _order.preparations[i].height), new Vector2(0.5f, 0.5f), 100.0f);
             ingredientImage.sprite = ingredientSprite;
+            preparationImage.sprite = preparationSprite;
+
+            overallHeight += _order.ingredients[i].height;
         }
 
-        Sprite barCodeSprite = Sprite.Create(order.barCode, new Rect(0, 0, 400, 100), new Vector2(0.5f, 0.5f), 100.0f);
-        barCode.sprite = barCodeSprite;
-        barCode.color = Color.white;
-        
-        barCodeNumber.text = order.barCodeNumber;
+        RectTransform rt = ingredients.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, overallHeight + (_numIngredients - 1) * 5);
 
-        Sprite signatureSprite = Sprite.Create(order.signature, new Rect(0, 0, 400, 100), new Vector2(0.5f, 0.5f), 100.0f); 
-        signature.sprite = signatureSprite;
-        signature.color = Color.white;
     }
 
     public void DeleteReceipt()
--- Assets/Scenes/Testing/Order Display Object/Item.cs
+++ Assets/Scenes/Testing/Order Display Object/Item.cs
@@ -4,36 +4,21 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public Texture2D barCode;
-    public String barCodeNumber;
     public List<Texture2D> ingredients;
+    public List<Texture2D> preparations;
     public float itemCost;
     public String itemName;
     public Texture2D foodPicture;
-    public Texture2D signature;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        barCodeNumber = generateBarCodeNumber();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private String generateBarCodeNumber()
-    {
-        System.Random rnd = new System.Random();
-        String output = "";
-
-        for (int i=0; i<9; i++)
-        {
-            output += rnd.Next(0, 10).ToString();
-        }
-
-        return output;
     }
 }
