1d4745eeefa2d86b346cea35166bfce44343e614
AdamLevin7 <adamarilevin@gmail.com>
2026-04-28 23:45:21 -0700

    basic guard animation in

--- Assets/Scenes/Testing/Refusal Button/RefusalButton.cs
+++ Assets/Scenes/Testing/Refusal Button/RefusalButton.cs
@@ -5,12 +5,20 @@ public class RefusalButton : MonoBehaviour
 {
     public Animator anim;
     public UnityEvent buttonPress;
+    public GameObject guardsPrefab;
+    public GameObject guardSpawnpoint;
+    public GameObject customerToRemove;
+    public float guardMoveDuration = 1f;
+    public float guardPauseAtCustomer = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         buttonPress.AddListener(GameObject.FindGameObjectWithTag("LogicManager").GetComponent<LogicManager>().ResizeButton);
         buttonPress.AddListener(AnimateButton);
+        buttonPress.AddListener(RemoveCustomer);
+        guardSpawnpoint = GameObject.FindGameObjectWithTag("Guard Spawnpoint");
+        customerToRemove = GameObject.FindGameObjectWithTag("Customer Refusal Test");
     }
 
     // Update is called once per frame
@@ -28,5 +36,50 @@ public class RefusalButton : MonoBehaviour
     public void AnimateButton()
     {
         anim.SetTrigger("Button Pressed");
+    }
+
+    public void RemoveCustomer()
+    {
+        if (guardsPrefab != null && guardSpawnpoint != null && customerToRemove != null)
+        {
+            GameObject guards = Instantiate(guardsPrefab, guardSpawnpoint.transform.position, Quaternion.identity);
+            StartCoroutine(GlideGuardToCustomerAndReturn(guards));
+        }
+        else if (customerToRemove == null){
+            Debug.Log("Customer to remove does not exist.");
+        }
+        else
+        {
+            Debug.LogError("One or more required objects are not assigned in the inspector.");
+        }
+    }
+
+    private System.Collections.IEnumerator GlideGuardToCustomerAndReturn(GameObject guards)
+    {
+        Vector3 startPosition = guardSpawnpoint.transform.position;
+        Vector3 customerPosition = customerToRemove.transform.position;
+
+        float elapsed = 0f;
+        while (elapsed < guardMoveDuration)
+        {
+            guards.transform.position = Vector3.Lerp(startPosition, customerPosition, elapsed / guardMoveDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        guards.transform.position = customerPosition;
+        yield return new WaitForSeconds(guardPauseAtCustomer);
+        Destroy(customerToRemove);
+        guards.GetComponent<Renderer>().material.color = Color.red;
+
+        elapsed = 0f;
+        while (elapsed < guardMoveDuration)
+        {
+            guards.transform.position = Vector3.Lerp(customerPosition, startPosition, elapsed / guardMoveDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        guards.transform.position = startPosition;
     }
 }
