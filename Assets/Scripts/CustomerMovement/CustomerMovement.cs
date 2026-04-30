0c67513559c96d6b1a760566272d0b23553a9513
Neel Dharm <dharmneel21@gmail.com>
2026-04-29 23:33:16 -0700

    added walk off behavior

--- Assets/Scripts/CustomerMovement/CustomerMovement.cs
+++ Assets/Scripts/CustomerMovement/CustomerMovement.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using System.Collections;
+
 public class CustomerMovement : MonoBehaviour
 {
-    //How many times the coroutine plays (how many frames in between start and end of the walk)
-    private float _duration = 6f;
+    //How many times the coroutine plays
+    private float _walkDuration = 6f;
     //how many times the customer should go up and down
     private float _bobs = 3;
     //how high the character should bob
@@ -21,14 +22,22 @@ public class CustomerMovement : MonoBehaviour
     //the y offset of the walk bobbing up and down
     private float _yOffset = 0f;
     //the X position the people want the customer to end at
-    public float Xposition = 0f;
+    public float XMainPosition = 0f;
+
+    // Arbitrary position outside the camera (how should we account for whether or not the camera
+    // isn't the same size? or do we not care?)
+    private float XEndPosition = 20f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //Beautiful thing about this is it doesn't matter the intial position of
         //the off screen character it will still arrive at the same spot.
-        _targetPos = new Vector3(Xposition,transform.position.y,transform.position.z);
+        _targetPos = new Vector3(XMainPosition,transform.position.y,transform.position.z);
         _startPos = transform.position;
+
+        // Setting character position to Main position just to test walking off
+        // transform.position = _targetPos;
 
         //for now I just put WalkOnScreen() here in order to test it
         WalkOnScreen();
@@ -49,7 +58,9 @@ public class CustomerMovement : MonoBehaviour
     //this method should just basically be a copy of above just it goes off screen. so change start and target position
     void WalkOffScreen()
     {
-        
+        _targetPos = new Vector3(XEndPosition,transform.position.y,transform.position.z);
+        _startPos = transform.position;
+        StartCoroutine(Walk());
     }
 
     //How I make it wait in between every "frame"
@@ -57,10 +68,10 @@ public class CustomerMovement : MonoBehaviour
     private IEnumerator Walk()
     {
         _elapsed = 0f;
-        while (_elapsed < _duration)
+        while (_elapsed < _walkDuration)
         {
             _elapsed += Time.deltaTime;
-            _percent = _elapsed/_duration;
+            _percent = _elapsed/_walkDuration;
 
             //calculates the movement necessary
             _newX = Mathf.Lerp(_startPos.x,_targetPos.x,_percent);
@@ -69,8 +80,8 @@ public class CustomerMovement : MonoBehaviour
             transform.position = new Vector3(_newX,_startPos.y + _yOffset,_startPos.z);
             //waits a frame
             yield return null;
-            
         }
+
         //fixes any float offset since floats aren't perfect.
         transform.position = _targetPos;
     }
