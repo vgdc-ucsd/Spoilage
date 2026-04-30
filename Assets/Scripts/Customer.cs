c17f05ff5e6656276ae54b5a69bf0ae095ada8d0
OP-idk <ojparsley@gmail.com>
2026-04-29 22:38:10 -0700

    Apply offset in Customer.cs

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -76,5 +76,7 @@ public class Customer : MonoBehaviour
             transform.Find("Sprites/SPOILAGE_FRONT").GetComponent<SpriteRenderer>().enabled = false;
             transform.Find("Sprites/SPOILAGE_BACK").GetComponent<SpriteRenderer>().enabled = false;
         }
+
+        transform.Find("Sprites/FACIAL_FEATURES").position = customerData.faceOffset;
     }
 }
