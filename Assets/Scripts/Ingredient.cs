a3be3e8f245db136d18e50c3ae3351464c834bc3
Nathan Scott <122716087+ncscott95@users.noreply.github.com>
2026-05-02 15:41:51 -0700

    delete redundant class definitions

--- Assets/Scripts/Ingredient.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Ingredient", menuName = "ScriptableObjects/Ingredient", order = 1)]
-public class Ingredient : ScriptableObject
-{
-    public string IngredientName;
-    public Sprite IngredientSprite;
-}
