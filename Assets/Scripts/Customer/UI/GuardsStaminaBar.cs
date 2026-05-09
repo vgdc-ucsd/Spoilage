3e7dd6f0d891b721a83fdf86e17beabf7383f47e
AdamLevin7 <adamarilevin@gmail.com>
2026-05-08 21:54:51 -0700

    cheese rotates when refusal button pressed
    cheese rotates around anchor point.

--- Assets/Scripts/Customer/UI/GuardsStaminaBar.cs
+++ Assets/Scripts/Customer/UI/GuardsStaminaBar.cs
@@ -4,26 +4,44 @@ using UnityEngine.UI;
 public class GuardsStaminaBar : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private Image FillImage;
-    public float fillRate = 0.01f;
-    public float buttonPressedReduceAmount = 0.5f;
+    //private RawImage FillImage;
+    public int maxPresses = 5;
+    public float buttonPressedReduceAmount;
+    private float pressesUsed;
+    public float radialAngle = 180f;
+    public float startAngle = -90f;
+    public RectTransform rectTransform;
     void Start()
     {
-        FillImage = GetComponent<Image>();
+        rectTransform = GetComponent<RectTransform>();
+        //FillImage = GetComponent<RawImage>();
+        buttonPressedReduceAmount = 1.0f / maxPresses;
+        pressesUsed = 0;
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        FillImage.fillAmount += fillRate * Time.deltaTime;
-    }
     public void buttonPressed()
     {
-        FillImage.fillAmount -= buttonPressedReduceAmount;
-
-        if(FillImage.fillAmount <= 0)
+        /*if(FillImage.fillAmount <= 0)
         {
-            Debug.Log("Guard is exhausted!");
+            Debug.Log("Guard is too eepy to move!");
+        }*/
+        //FillImage.fillAmount -= buttonPressedReduceAmount;
+        pressesUsed++;
+        if(pressesUsed > maxPresses)
+        {
+            Debug.Log("Guard is too eepy to move!");
+            return;
         }
+        float rotAngle = pressesUsed / maxPresses * radialAngle + startAngle;
+        rectTransform.rotation = Quaternion.Euler(0, 0, rotAngle);
+
+    }
+    public void setMaxPresses(int newMaxPresses)
+    {
+        maxPresses = newMaxPresses;
+        buttonPressedReduceAmount = 1.0f / maxPresses;
+        pressesUsed = 0;
     }
 }
