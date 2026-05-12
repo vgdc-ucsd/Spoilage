c5569f96c192d1d9c78236a3e1f5f7ad5182771a
gloomybear300 <s2hill@ucsd.edu>
2026-05-11 17:35:14 -0700

    fix buttons
    fixed the pause scene in unelegant way. There is now a pausemenu singleton, with a pauseUI that is turned active and inactive. Load settings button does not work, resume button does workk.

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -1,32 +1,56 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : Singleton<PauseMenu>
 {
     public static bool GameIsPaused = false;
     public GameObject PauseUI;
 
+    public GameObject ResumeButton;
+    public GameObject SettingsButton;
+    public GameObject MainMenuButton; 
+
+    void Start()
+    {
+        if (ResumeButton != null)
+            ResumeButton.GetComponent<Button>().onClick.AddListener(Resume);
+        
+        if (SettingsButton != null)
+            SettingsButton.GetComponent<Button>().onClick.AddListener(LoadSettings);
+        
+        if (MainMenuButton != null)
+            MainMenuButton.GetComponent<Button>().onClick.AddListener(ReturnToMenu);
+        
+    }
     void Update()
     {
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
         {
-            if (GameIsPaused)
+            if (!GameIsPaused)
             {
-                Resume();
+                UnityEngine.Debug.Log("Pause");
+                
+                //SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+                Pause();
             }
             else
             {
-                Pause();
+                UnityEngine.Debug.Log("Resume");
+                
+                //SceneManager.UnloadSceneAsync("PauseMenu");
+                Resume();
             }
         }
     }
 
     public void Resume()
     {
+        
         GameIsPaused = false;
-        PauseUI.SetActive(false);
         Time.timeScale = 1f;
+        PauseUI.SetActive(false);
     }
 
     public void LoadSettings()
@@ -41,13 +65,16 @@ public class PauseMenu : MonoBehaviour
 
     void Pause()
     {
-        GameIsPaused = true;
         PauseUI.SetActive(true);
+        GameIsPaused = true;
         Time.timeScale = 0f;
     }
 
     public void ReturnToMenu()
     {
+
         GameManager.Instance.Load(GameScene.MAIN_MENU);
     }
 }
+
+
