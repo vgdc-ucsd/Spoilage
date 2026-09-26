abcd2b9f75c2c88ca8d20738fe83090e9607d980
faern1s <lemniscaticwind@gmail.com>
2026-09-25 18:20:34 -0700

    Uploaded Recipe Book text
    recipe book + changed some of the spgahetti :9

--- Assets/Scripts/Cooking/Ingredients/Domain/IngredientData.cs
+++ Assets/Scripts/Cooking/Ingredients/Domain/IngredientData.cs
@@ -4,12 +4,13 @@
 public sealed class IngredientData : ScriptableObject
 {
     [SerializeField] private string _name;
-
     [SerializeField] private float _spoilTime = 15f;
     [SerializeField] private Sprite _normalSprite;
     [SerializeField] private Sprite _spoiledSprite;
     [SerializeField] private Sprite _plateSprite;
     [SerializeField] private bool _isSmallIngredient;
+    [TextArea(5, 40)] [SerializeField] private string _recipeBookDescriptionUnspoiled;
+    [TextArea(5, 40)] [SerializeField] private string _recipeBookDescriptionSpoiled;
 
     public string Name => _name;
     public float SpoilTime => _spoilTime;
@@ -17,4 +18,6 @@ public sealed class IngredientData : ScriptableObject
     public Sprite SpoiledSprite => _spoiledSprite;
     public Sprite PlateSprite => _plateSprite;
     public bool IsSmallIngredient => _isSmallIngredient;
+    public string RecipeBookDescriptionUnspoiled => _recipeBookDescriptionUnspoiled;
+    public string RecipeBookDescriptionSpoiled => _recipeBookDescriptionSpoiled;
 }
